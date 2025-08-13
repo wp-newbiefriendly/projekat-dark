00c240ec45202e09371688752e5b1316ab544481
milan <milanmastervisacard@gmail.com>
2025-08-13 10:23:49 +0200

    updatee

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/prognoza', function () {
+    return view('prognoza');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
