0a63826118d769fae7fa9240f3a9a2fc0682ef5d
milan <milanmastervisacard@gmail.com>
2025-08-19 13:04:08 +0200

    Dodato: Choose ROLE (ask ili choice varijacije)

--- database/seeders/AddUser.php
+++ database/seeders/AddUser.php
@@ -11,7 +11,7 @@ class AddUser extends Seeder
     public function run(): void
     {
         // Unesi email - **required**
-        $email = $this->command->ask('Unesite email:');
+        $email = $this->command->ask('Unesite email');
         if (empty($email)) {
             $this->command->error('❌ Niste uneli email!');
             return; // prekidamo operaciju
@@ -22,23 +22,34 @@ class AddUser extends Seeder
             return;
         }
         // Unesi username - **required**
-        $username = $this->command->ask('Unesite username:');
+        $username = $this->command->ask('Unesite username');
         if (empty($username)) {
             $this->command->error('❌ Niste uneli username!');
             return;
         }
         // Unesi password - **required**
-        $password = $this->command->ask('Unesite lozinku:');
+        $password = $this->command->ask('Unesite lozinku');
         if (empty($password)) {
             $this->command->error('❌ Niste uneli lozinku!');
             return;
+        }
+        // Izaberi role - ask ili choice **required
+        // Varijanta ->choice - izaberi ponudjenje opcije | $role = $this->command->choice('Izaberite role:', ['admin', 'user']);
+        $role = $this->command->ask("Unesite role (admin ili user)");
+        if (empty($role)) {
+            $this->command->error("Niste uneli role!");
+                return;
+        }
+            if (!in_array($role, ['admin', 'user'])) {
+                $this->command->error("Role mora biti admin ili user");
+                return;
         }
 
         User::create([
             'name' => $username,
             'email' => $email,
             'password' => Hash::make($password),
-            'role' => 'user',
+            'role' => $role,
         ]);
 
         $this->command->info("✔ User '$email' uspešno dodat u bazu.");
