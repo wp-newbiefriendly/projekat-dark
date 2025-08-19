6dfb180de65c7659fea18c776dac4cb2530ee93d
milan <milanmastervisacard@gmail.com>
2025-08-19 23:44:28 +0200

    Domaci #12 - promena city u city_id(cities->id)

--- database/migrations/2025_08_19_213815_update_weather_table.php
+++ database/migrations/2025_08_19_213815_update_weather_table.php
@@ -6,23 +6,28 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('weather', function (Blueprint $table) {
-            //
-        });
+            // obriši staru kolonu "city" ako postoji
+            if (Schema::hasColumn('weather', 'city')) {
+                $table->dropColumn('city');
+            }
+
+            // dodaj novu kolonu "city_id"
+            $table->unsignedBigInteger('city_id')->after('id');
+
+            // foreign key ka cities tabeli
+            $table->foreign('city_id')->references('id')->on('cities')->onDelete('cascade');
+            }
+        );
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('weather', function (Blueprint $table) {
-            //
+            $table->dropForeign(['city_id']);
+            $table->dropColumn('city_id');
         });
     }
 };
