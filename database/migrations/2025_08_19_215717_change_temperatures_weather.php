2ed2daa3249cc06991622453816d66f50e8ef315
milan <milanmastervisacard@gmail.com>
2025-08-20 00:05:19 +0200

    Domaci #12 - promena temperatures u temperature

---
+++ database/migrations/2025_08_19_215717_change_temperatures_weather.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('weather', function (Blueprint $table) {
+            if (Schema::hasColumn('weather', 'temperature')) {
+                $table->dropColumn('temperature');
+            }
+
+            $table->float('temperature')->after('city_id');
+        }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
