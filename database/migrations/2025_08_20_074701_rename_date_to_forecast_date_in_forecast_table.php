c2f0b04f923700411b75251d45c4d5d7f2e6fa57
milan <milanmastervisacard@gmail.com>
2025-08-20 09:52:48 +0200

    Domaci #12 - Renamed forecasts(date) to "forecast_date", installed "composer require doctrine/dbal"

---
+++ database/migrations/2025_08_20_074701_rename_date_to_forecast_date_in_forecast_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('forecasts', function (Blueprint $table) {
+            $table->renameColumn('date', 'forecast_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('forecasts', function (Blueprint $table) {
+            $table->renameColumn('forecast_date', 'date');
+        });
+    }
+};
