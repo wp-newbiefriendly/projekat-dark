ebe4e8e105dc4e6c4adda1ed291027adc68251ad
milan <milanmastervisacard@gmail.com>
2025-09-13 15:48:55 +0200

    Domaci #17.04 - model UserCitiesModel - protected+belongs

--- app/Models/UserCitiesModel.php
+++ app/Models/UserCitiesModel.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserCitiesModel extends Model
 {
-    //
+    protected $table = 'user_cities';
+
+    protected $fillable = ['city_id', 'user_id'];
+}
+
+public function usercitiesCities()
+{
+    return $this->belongsTo(CitiesModel::class, 'city_id');
+}
+public function usercitiesUsers()
+{
+    return $this->belongsTo(User::class, 'user_id');
 }
